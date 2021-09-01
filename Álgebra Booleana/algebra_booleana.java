f98d3e722344a89004373cfec6ced5df338e7901
Camila <cacagrandini@gmail.com>
2021-09-01 18:18:31 -0300

    initial commit with 5 exercises

---
+++ Álgebra Booleana/algebra_booleana.java
@@ -0,0 +1,8 @@
+class algebra_booleana {
+    public static void main (String[] args){
+        String palavra="";
+        palavra = MyIO.readLine();
+        palavra.replace("a","o");
+        System.out.println(palavra);
+    }
+}
